7eb36163a1d75c44591ae56c5adfdfe702c017b1
adraax <merlin.olivier71@gmail.com>
2015-11-09 11:35:04 +0100

    Modif fenêtre de connexion
    ouvre le programme si tu veux voir ce que ça fait

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,60 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.conInscrTabControl = new System.Windows.Forms.TabControl();
+            this.connexionTabPage = new System.Windows.Forms.TabPage();
+            this.inscriptionTabPage = new System.Windows.Forms.TabPage();
+            this.usernameLabelConnexion = new System.Windows.Forms.Label();
+            this.usernameConnexion = new System.Windows.Forms.TextBox();
+            this.passwordLabelConnexion = new System.Windows.Forms.Label();
+            this.passwordConnexion = new System.Windows.Forms.TextBox();
             this.connexion = new System.Windows.Forms.Button();
-            this.inscription = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.conInscrTabControl.SuspendLayout();
+            this.connexionTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // conInscrTabControl
             // 
-            this.panel1.AutoSize = true;
-            this.panel1.Controls.Add(this.inscription);
-            this.panel1.Controls.Add(this.connexion);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(321, 160);
-            this.panel1.TabIndex = 0;
+            this.conInscrTabControl.Controls.Add(this.connexionTabPage);
+            this.conInscrTabControl.Controls.Add(this.inscriptionTabPage);
+            this.conInscrTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.conInscrTabControl.Location = new System.Drawing.Point(0, 0);
+            this.conInscrTabControl.Name = "conInscrTabControl";
+            this.conInscrTabControl.SelectedIndex = 0;
+            this.conInscrTabControl.Size = new System.Drawing.Size(321, 160);
+            this.conInscrTabControl.TabIndex = 3;
+            // 
+            // connexionTabPage
+            // 
+            this.connexionTabPage.Controls.Add(this.connexion);
+            this.connexionTabPage.Controls.Add(this.passwordConnexion);
+            this.connexionTabPage.Controls.Add(this.passwordLabelConnexion);
+            this.connexionTabPage.Controls.Add(this.usernameConnexion);
+            this.connexionTabPage.Controls.Add(this.usernameLabelConnexion);
+            this.connexionTabPage.Location = new System.Drawing.Point(4, 22);
+            this.connexionTabPage.Name = "connexionTabPage";
+            this.connexionTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.connexionTabPage.Size = new System.Drawing.Size(313, 134);
+            this.connexionTabPage.TabIndex = 0;
+            this.connexionTabPage.Text = "Connexion";
+            this.connexionTabPage.UseVisualStyleBackColor = true;
+            // 
+            // inscriptionTabPage
+            // 
+            this.inscriptionTabPage.Location = new System.Drawing.Point(4, 22);
+            this.inscriptionTabPage.Name = "inscriptionTabPage";
+            this.inscriptionTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.inscriptionTabPage.Size = new System.Drawing.Size(313, 134);
+            this.inscriptionTabPage.TabIndex = 1;
+            this.inscriptionTabPage.Text = "Inscription";
+            this.inscriptionTabPage.UseVisualStyleBackColor = true;
+            // 
+            // usernameLabelConnexion
+            // 
+            this.usernameLabelConnexion.AutoSize = true;
+            this.usernameLabelConnexion.Location = new System.Drawing.Point(104, 3);
+            this.usernameLabelConnexion.Name = "usernameLabelConnexion";
+            this.usernameLabelConnexion.Size = new System.Drawing.Size(90, 13);
+            this.usernameLabelConnexion.TabIndex = 0;
+            this.usernameLabelConnexion.Text = "Nom d\'utilisateur :";
+            // 
+            // usernameConnexion
+            // 
+            this.usernameConnexion.Location = new System.Drawing.Point(107, 19);
+            this.usernameConnexion.Name = "usernameConnexion";
+            this.usernameConnexion.Size = new System.Drawing.Size(100, 20);
+            this.usernameConnexion.TabIndex = 1;
+            // 
+            // passwordLabelConnexion
+            // 
+            this.passwordLabelConnexion.AutoSize = true;
+            this.passwordLabelConnexion.Location = new System.Drawing.Point(107, 46);
+            this.passwordLabelConnexion.Name = "passwordLabelConnexion";
+            this.passwordLabelConnexion.Size = new System.Drawing.Size(77, 13);
+            this.passwordLabelConnexion.TabIndex = 2;
+            this.passwordLabelConnexion.Text = "Mot de passe :";
+            // 
+            // passwordConnexion
+            // 
+            this.passwordConnexion.Location = new System.Drawing.Point(107, 63);
+            this.passwordConnexion.Name = "passwordConnexion";
+            this.passwordConnexion.PasswordChar = '*';
+            this.passwordConnexion.Size = new System.Drawing.Size(100, 20);
+            this.passwordConnexion.TabIndex = 3;
             // 
             // connexion
             // 
-            this.connexion.Location = new System.Drawing.Point(126, 36);
+            this.connexion.Location = new System.Drawing.Point(107, 90);
             this.connexion.Name = "connexion";
-            this.connexion.Size = new System.Drawing.Size(75, 23);
-            this.connexion.TabIndex = 0;
+            this.connexion.Size = new System.Drawing.Size(100, 23);
+            this.connexion.TabIndex = 4;
             this.connexion.Text = "Connexion";
             this.connexion.UseVisualStyleBackColor = true;
             this.connexion.Click += new System.EventHandler(this.connexion_Click);
-            // 
-            // inscription
-            // 
-            this.inscription.Location = new System.Drawing.Point(126, 78);
-            this.inscription.Name = "inscription";
-            this.inscription.Size = new System.Drawing.Size(75, 23);
-            this.inscription.TabIndex = 1;
-            this.inscription.Text = "Inscription";
-            this.inscription.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(321, 160);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.conInscrTabControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainWindow";
             this.Text = "MainWindow";
-            this.panel1.ResumeLayout(false);
+            this.conInscrTabControl.ResumeLayout(false);
+            this.connexionTabPage.ResumeLayout(false);
+            this.connexionTabPage.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button inscription;
+        private System.Windows.Forms.TabControl conInscrTabControl;
+        private System.Windows.Forms.TabPage connexionTabPage;
+        private System.Windows.Forms.TabPage inscriptionTabPage;
+        private System.Windows.Forms.TextBox usernameConnexion;
+        private System.Windows.Forms.Label usernameLabelConnexion;
+        private System.Windows.Forms.Label passwordLabelConnexion;
         private System.Windows.Forms.Button connexion;
+        private System.Windows.Forms.TextBox passwordConnexion;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using System.Data.Common;
+using System.Diagnostics;
 
 namespace Main_project_VERON_MERLIN
 {
@@ -20,8 +24,33 @@ namespace Main_project_VERON_MERLIN
         private void connexion_Click(object sender, EventArgs e)
         {
             this.Visible = false;
-            Connexion fenetreConnexion = new Connexion();
-            fenetreConnexion.ShowDialog();
+
+            #region Accès BD
+            OracleConnection connec = new OracleConnection();
+            connec.ConnectionString = "User Id=om141055; Password=om141055; Data Source=//ufrsciencestech.u-bourgogne.fr:25559/ense2015";
+            DbProviderFactory dbpf;
+            dbpf = DbProviderFactories.GetFactory("Oracle.DataAccess.Client");
+
+            connec.Open();
+
+            DbDataAdapter dba;
+            // initialisation de la connexion conn et de dbpf
+            dba = dbpf.CreateDataAdapter();
+            dba.SelectCommand = connec.CreateCommand();
+            dba.SelectCommand.CommandText = $"SELECT * FROM PROJET_IHM_USERS WHERE USERNAME = '{usernameConnexion.Text}' AND PASSWORD = '{passwordConnexion.Text}' AND ACTIVE = 1";
+            DataSet ds = new DataSet();
+            dba.Fill(ds, "Users");
+
+            Trace.WriteLine(ds.Tables["Users"].Rows.Count);
+
+            #endregion
+
+            if (ds.Tables["Users"].Rows.Count == 1)
+            {
+                this.Visible = false;
+                Accueil_user fenetreAccueilUser = new Accueil_user(new User(int.Parse(ds.Tables["Users"].Rows[0]["ID"].ToString()), ds.Tables["Users"].Rows[0]["USERNAME"].ToString()));
+                fenetreAccueilUser.ShowDialog();
+            }
             this.Dispose();
         }
     }
